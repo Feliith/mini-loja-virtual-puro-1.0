3ea355fd1fc66f46bc862b70b517fd6bb7799fb0
unknown <felipelmrobles03@gmail.com>
2022-02-21 11:57:30 -0300

    bug solved

--- app.js
+++ app.js
@@ -2,20 +2,20 @@
 const items = [
     {
         title: 'Camiseta',
-        price: 'R$49,99',
-        url: 'img/t-shirt-0',
+        price: 49.99,
+        url: 'img/t-shirt',
         qnt: 0
     },
     {
         title: 'Moletom',
-        price: 'R$69,99',
-        url: 'img/sweatshirt-0',
+        price: 69.99,
+        url: 'img/sweatshirt',
         qnt: 0
     },
     {
         title: 'Calça',
-        price: 'R$79,89',
-        url: 'img/pants-0',
+        price: 79.89,
+        url: 'img/pants',
         qnt: 0
     }
 ]
@@ -26,23 +26,15 @@ const Store = () => {
     items.map((props, index) => {
         productsConteiner.innerHTML += `
             <div class="product-single">
-                <button class="slider-button left" onClick="leftBtn(${index})">
-                    <img src="img/icons/arrows-left.png"/>
-                </button>
-                <button class="slider-button right" onClick="rightBtn(${index})">
-                    <img src="img/icons/arrows-right.png"/>
-                </button>
                 <div class="slider">
                     <div class="product-slider">
                         <span class="white-ball"></span>
                         <span class="white-ball-shadow"></span>
-                        <img src="${props.url + 1}.png" class="product-image"></img>
-                        <img src="${props.url + 2}.png" class="product-image"></img>
-                        <img src="${props.url + 3}.png" class="product-image"></img>
+                        <img src="${props.url}.png" class="product-image"></img>
                     </div>
                 </div>
-                <h3 class="product-name">${props.title} - ${props.price}</h3>
-                <button key="${index}" class="cart-btn" onClick="getID(this)">Adicionar ao carrinho</button>
+                <h3 class="product-name">${props.title} - R$${props.price}</h3>
+                <button class="cart-btn" onClick="getID(${index}, ${props.price})">Adicionar ao carrinho</button>
             </div>
         `
     })
@@ -59,80 +51,50 @@ updateCart = () => {
             cartConteiner.innerHTML += `
                 <div class="single-conteiner-cart">
                     <div class="product-cart">
-                        <img src="${props.url + (pos[index] + 1)}.png" class="product-image cart"></img>
+                        <img src="${props.url}.png" class="product-image cart"></img>
                     </div>
-                    <div>
-                        <h3 class="product-name">${props.title}  x${props.qnt}</h3>
-                        <h3 class="product-name">Cor: ${setColor(index)}</h3>
+                    <div class="product-single-title">
+                        <h3 class="product-name cart">${props.title}</h3>
+                        <h3 class="product-name cart">R$${props.price}</h3>
+                        <div class="product-quant">
+                            <button class="product-btn sub" onClick="productSub(${index}, ${props.price})">-</button>
+                            <p>${props.qnt}</p>
+                            <button class="product-btn add" onClick="productAdd(${index}, ${props.price})">+</button>
+                        </div>
+                        <button class="remove-all" onClick="removeAll(${index}, ${props.price})"><img src="img/icons/x.png"/></button>
                     </div>
                 </div>
             `
         }
     })
+    totalPriceSpan.innerText = `R$${parseFloat(totalPrice.toFixed(2))}`
 }
 
-setColor = (id) => {
-    if (pos[id] == 0) {
-        return 'Preto'
-    } else if (pos[id] == 1) {
-        return 'Verde'
-    } else {
-        return 'Vermelho'
-    }
-}
-
-getID = (btn) => {
-    let key = btn.getAttribute('key')
-    items[key].qnt++
+getID = (id, price) => {
+    items[id].qnt++
+    totalPrice += price
     updateCart()
 }
 
-
-
-const productSlider = document.querySelectorAll('.product-slider')
-
-let pos = [0 , 0, 0]
-
-leftBtn = (id) => {
-    if (pos[id] == 0) {
-        pos[id] = 2
-        productSlider[id].style.marginLeft = `${-250 * pos[id]}px`
-    } else {
-        pos[id]--
-        productSlider[id].style.marginLeft = `${-250 * pos[id]}px`
-    }
+productSub = (id, price) => {
+    items[id].qnt--
+    totalPrice -= price
+    updateCart()
 }
 
-rightBtn = (id) => {
-    if (pos[id] == 2) {
-        pos[id] = 0
-        productSlider[id].style.marginLeft = `${-250 * pos[id]}px`
-    } else {
-        pos[id]++
-        productSlider[id].style.marginLeft = `${-250 * pos[id]}px`
-    }
+productAdd = (id, price) => {
+    items[id].qnt++
+    totalPrice += price
+    updateCart()
 }
 
-
-const leftButton = document.querySelectorAll('.slider-button.left')
-const rightButton = document.querySelectorAll('.slider-button.right')
-const whiteBall = document.querySelectorAll('.white-ball-shadow')
-const quantProducts = document.querySelectorAll('.product-single')
-
-for (let c = 0 ; c < quantProducts.length ; c++) {
-    leftButton[c].addEventListener('mouseenter', () => {
-        whiteBall[c].style.background = 'linear-gradient(90deg, #cfcece, transparent, transparent)'
-    })
-    
-    leftButton[c].addEventListener('mouseleave', () => {
-        whiteBall[c].style.background = 'none'
-    })
-
-    rightButton[c].addEventListener('mouseenter', () => {
-        whiteBall[c].style.background = 'linear-gradient(-90deg, #cfcece, transparent, transparent)'
-    })
-    
-    rightButton[c].addEventListener('mouseleave', () => {
-        whiteBall[c].style.background = 'none'
-    })
+removeAll = (id, price) => {
+    totalPrice -= items[id].qnt * price
+    items[id].qnt = 0
+    updateCart()
 }
+
+const totalPriceConteiner = document.querySelector('.total-price')
+const totalPriceSpan = document.querySelector('.total-price > span')
+
+let totalPrice = 0
